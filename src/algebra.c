5a6b1fcea7203507c77c8bfd4cd298d5f30b9fd7
Ck0224 <3316793278@qq.com>
2024-07-04 14:36:28 +0800

    Update algebra.c

--- src/algebra.c
+++ src/algebra.c
@@ -13,6 +13,11 @@ Matrix create_matrix(int row, int col)
 Matrix add_matrix(Matrix a, Matrix b)
 {
     // ToDo
+    if (a.cols!=b.cols||a.rows!=b.rows)
+    {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
     Matrix m;
     for (int i = 0; i < a.rows; i++)
         for (int j = 0; j < a.cols; j++)
@@ -27,6 +32,11 @@ Matrix add_matrix(Matrix a, Matrix b)
 Matrix sub_matrix(Matrix a, Matrix b)
 {
     // ToDo
+    if (a.cols!=b.cols||a.rows!=b.rows)
+    {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
     Matrix m;
     for (int i = 0; i < a.rows; i++)
         for (int j = 0; j < a.cols; j++)
@@ -41,25 +51,58 @@ Matrix sub_matrix(Matrix a, Matrix b)
 Matrix mul_matrix(Matrix a, Matrix b)
 {
     // ToDo
-    return create_matrix(0, 0);
+    if (a.cols!=b.rows)
+    {
+        printf("Error: The number of cols of matrix a must be equal to the number of rows of matrix b.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix m;
+    m.rows=a.rows,m.cols=b.cols;
+	for(int i=0; i<m.rows; i++)
+		for(int j=0; j<m.cols; j++) {
+			m.data[i][j]=0;
+			int k=0;
+			while(k<a.cols)
+				m.data[i][j]+=a.data[i][k]*b.data[k][j],k++;
+		}
+    return m;
 }
 
 Matrix scale_matrix(Matrix a, double k)
 {
     // ToDo
-    return create_matrix(0, 0);
+    Matrix c;
+    for (int i = 0; i < a.rows; i++)
+        for (int j = 0; j < a.cols; j++)
+        {
+           c.data[i][j]=a.data[i][j]*k;
+        }
+    c.cols=a.cols;
+    c.rows=a.rows;
+    return c;
 }
 
 Matrix transpose_matrix(Matrix a)
 {
     // ToDo
-    return create_matrix(0, 0);
+    Matrix c; 
+    c.rows=a.cols; 
+    c.cols=a.rows; 
+    for (int i = 0; i < a.rows; i++)
+        for(int j = 0; j < a.cols; j++)
+            c.data[j][i] = a.data[i][j];
+    return c;
 }
 
 double det_matrix(Matrix a)
 {
     // ToDo
-    return 0;
+    if (a.cols!=a.rows)
+    {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0;
+    }
+    
 }
 
 Matrix inv_matrix(Matrix a)
